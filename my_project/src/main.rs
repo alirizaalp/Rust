b8a8263bc8a2f48fc3a9ecf2d2021c0f3b5433e2
alirizaalp <alralp@outlook.com>
2023-12-11 21:01:40 +0300

    second commit

--- my_project/src/main.rs
+++ my_project/src/main.rs
@@ -1,20 +1,67 @@
+use std::io;
 
-fn concatenate_strings(str1: &str, str2: &str) -> String {
-    let mut result = String::new();
-    result.push_str(str1);
-    result.push_str(str2);
-    result
+
+enum Operation {
+    Add(f64, f64),
+    Subtract(f64, f64),
+    Multiply(f64, f64),
+    Divide(f64, f64),
+}
+
+
+fn calculate(op: &Operation) -> f64 {
+
+    match op {
+        Operation::Add(x, y) => x + y,
+        Operation::Subtract(x, y) => x - y,
+        Operation::Multiply(x, y) => x * y,
+        Operation::Divide(x, y) => {
+            if *y != 0.0 {
+                x / y
+            } else {
+                println!("Error: Division by zero");
+                f64::NAN
+            }
+        }
+    }
 }
 
 fn main() {
-    
-    let string1 = String::from("Hello, ");
-    let string2 = String::from("Rust!");
+ 
+    println!("Enter the first number:");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read input");
+    let first_number: f64 = input.trim().parse().expect("Invalid input");
 
-   
-    let concatenated_string = concatenate_strings(&string1, &string2);
+  
+    println!("Enter the operation (+, -, *, /):");
+    let mut operation_input = String::new();
+    io::stdin().read_line(&mut operation_input).expect("Failed to read input");
+    let operation = operation_input.trim();
 
-    println!("{}", concatenated_string);
+
+    println!("Enter the second number:");
+    input.clear();
+    io::stdin().read_line(&mut input).expect("Failed to read input");
+    let second_number: f64 = input.trim().parse().expect("Invalid input");
+
+ 
+    let user_operation = match operation {
+        "+" => Operation::Add(first_number, second_number),
+        "-" => Operation::Subtract(first_number, second_number),
+        "*" => Operation::Multiply(first_number, second_number),
+        "/" => Operation::Divide(first_number, second_number),
+        _ => {
+            println!("Error: Invalid operation");
+            return;
+        }
+    };
+
+
+    let result = calculate(&user_operation);
+
+
+    println!("Result: {}", result);
 }
 
 
