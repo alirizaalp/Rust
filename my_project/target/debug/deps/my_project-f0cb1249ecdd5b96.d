0d3953ef6a52343c617817c91d4d74d48050f1f5
alirizaalp <alralp@outlook.com>
2023-12-11 20:44:09 +0300

    first commit

---
+++ my_project/target/debug/deps/my_project-f0cb1249ecdd5b96.d
@@ -0,0 +1,5 @@
+C:\Users\Asus\Desktop\rustnew\my_project\target\debug\deps\libmy_project-f0cb1249ecdd5b96.rmeta: src\main.rs
+
+C:\Users\Asus\Desktop\rustnew\my_project\target\debug\deps\my_project-f0cb1249ecdd5b96.d: src\main.rs
+
+src\main.rs:
---
+++ my_project/target/debug/deps/my_project-f4e853ccccb8fdf8.d
@@ -0,0 +1,5 @@
+C:\Users\Asus\Desktop\rustnew\my_project\target\debug\deps\libmy_project-f4e853ccccb8fdf8.rmeta: src\main.rs
+
+C:\Users\Asus\Desktop\rustnew\my_project\target\debug\deps\my_project-f4e853ccccb8fdf8.d: src\main.rs
+
+src\main.rs:
